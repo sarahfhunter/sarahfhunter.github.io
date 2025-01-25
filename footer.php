a892e2ba5f3846333b0cb60498f8a94926ec84e4
Sarah <sarahfaithbradshaw@gmail.com>
2025-01-24 19:29:52 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+function footer()
+{
+    echo '
+    <footer id="footer" class="wrapper style1-alt">
+        <div class="inner">
+            <ul class="menu">
+                <li>&copy; Untitled. All rights reserved.</li>
+                <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                <li><a href="www.linkedin.com/in/sarah-hunter-1b0916154" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+                <li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+                <!-- TODO: decide if I want github on here or nah -->
+            </ul>
+        </div>
+    </footer>';
+}
+footer();
+?>
